f41b83204839c2ebdd3d910cdb4513504018cf8f
6127744881 <54647876+6127744881@users.noreply.github.com>
2019-08-28 18:57:39 -0400

    Create AsciiBox2.java
    1st program file

---
+++ AsciiBox2.java
@@ -0,0 +1,30 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Amrita Pathak
+//Date -08/28/19
+//Class -1A
+//Lab  -1.1 ASCII BOXES
+
+public class AsciiBox2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Name: Amrita      Date:8/28/19\n\n");
+      
+      
+      System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAA");
+      System.out.println("+++++++++++++++++++++++++++");
+      System.out.println("+++                     +++");
+      System.out.println("+++                     +++");
+      System.out.println("+++                     +++");
+      System.out.println("+++                     +++");
+      System.out.println("+++     CompSci         +++");
+      System.out.println("+++                     +++");
+      System.out.println("+++                     +++");
+      System.out.println("+++                     +++");
+      System.out.println("+++                     +++");
+      System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAA");
+      System.out.println("+++++++++++++++++++++++++++");
+	}
+}
